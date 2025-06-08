baea6873b906da8d7a555ca1957457bbc460bb21
iandrei250 <aiordache020@gmail.com>
2025-06-08 17:08:14 +0300

    searchbar

--- portofolio/src/components/shared/MenuBar.tsx
+++ portofolio/src/components/shared/MenuBar.tsx
@@ -1,41 +1,19 @@
 import windowsIcon from "../../assets/windows.svg";
+import Searchbar from "./Searchbar";
 
 const MenuBar = () => {
   return (
     <div>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900 fixed bottom-0 left-0 w-full z-50">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between p-4">
+      <nav className="border-gray-200 bg-black fixed bottom-0 left-0 w-full z-50 h-[50px]">
+        <div className="max-w-screen-xl flex flex-wrap items-center">
           <div className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src={windowsIcon} className="h-8" alt="Flowbite Logo" />
+            <img
+              src={windowsIcon}
+              className="h-8  mx-[20px]"
+              alt="Flowbite Logo"
+            />
           </div>
-          <button
-            data-collapse-toggle="navbar-default"
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-default"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-          <div
-            className="hidden w-full md:block md:w-auto"
-            id="navbar-default"
-          ></div>
+          <Searchbar />
         </div>
       </nav>
     </div>
---
+++ portofolio/src/components/shared/Searchbar.tsx
@@ -0,0 +1,13 @@
+const Searchbar = () => {
+  return (
+    <div className="bg-white h-full mx-[20px] ">
+      <input
+        type="text"
+        className="p-0  h-full"
+        placeholder="Place your text here"
+      />
+    </div>
+  );
+};
+
+export default Searchbar;
